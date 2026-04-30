4c9d4061fe72de300138592bd425750c1a878056
Tejesh yewale <tejeshyewale917@gmail.com>
2026-04-30 13:42:58 +0530

    Update Instance.py

--- nodes/src/nodes/ml_sklearn/Instance.py
+++ nodes/src/nodes/ml_sklearn/Instance.py
@@ -1,24 +1,12 @@
-from typing import Any
-
 class Instance:
     def __init__(self, IGlobal):
         self.IGlobal = IGlobal
 
-    def open(self, obj: Any):
-        # Called when new object starts
-        pass
-
-    def process(self, data: Any):
-        # Run ML prediction
-        if not self.IGlobal.preprocessor:
-            return {'error': 'PreProcessor not initialized'}
-
+    def process(self, text):
         try:
-            result = self.IGlobal.preprocessor.run({'price': data})
-            return result
-        except Exception as e:
-            return {'error': str(e)}
+            if self.IGlobal.preprocessor:
+                return self.IGlobal.preprocessor.process(text)
+        except Exception:
+            pass
 
-    def close(self):
-        # Called when processing ends
-        pass
+        return text
