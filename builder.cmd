7734466ab8be828898777c92af58215a455cc95a
NIHAL NIHALANI <nihal.nihalani@gmail.com>
2026-05-01 04:53:46 +0530

    ci: serialize full tests on CI

--- builder.cmd
+++ builder.cmd
@@ -1,1 +1,16 @@
-@node "%~dp0scripts/build.js" %*
+@echo off
+setlocal
+
+set "EXTRA_ARGS="
+if defined CI (
+  set "HAS_TEST="
+  set "HAS_SEQUENTIAL="
+  for %%A in (%*) do (
+    if "%%~A"=="test" set "HAS_TEST=1"
+    if "%%~A"=="--sequential" set "HAS_SEQUENTIAL=1"
+    if "%%~A"=="-s" set "HAS_SEQUENTIAL=1"
+  )
+  if defined HAS_TEST if not defined HAS_SEQUENTIAL set "EXTRA_ARGS=--sequential"
+)
+
+node "%~dp0scripts/build.js" %* %EXTRA_ARGS%
