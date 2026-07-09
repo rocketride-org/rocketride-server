15dbbe5faa7bb52a8ac2b34fefd97ad039f0448a
Tejesh Yewale <tejeshyewale917@gmail.com>
2026-07-09 11:12:34 +0530

    fix(ml-sklearn): remove unused shutil import from tests

--- nodes/src/nodes/ml_sklearn/test_ml_sklearn.py
+++ nodes/src/nodes/ml_sklearn/test_ml_sklearn.py
@@ -4,7 +4,6 @@
 # =============================================================================
 import os
 import sys
-import shutil
 import joblib
 import numpy as np
 from sklearn.linear_model import LinearRegression
